b03010bfc5d4f45e6a92c0b48ff10dc4f3c16b9f
Aditya Keshav <adityakeshav393@gmail.com>
2025-03-31 17:37:13 +0530

    js

---
+++ JS/webapi.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+function timeout(){
+    console.log("Click the button");
+}
+
+setTimeout(timeout,1000);
+
+console.log("Welome ");
+
+console.log("hey");
+
+
+
+let c=0;
+for (let i=0;i<10;i++){
+    c++;
+}
+
+console.log(c);
