92f72738cab7581787fa96014e5fc7428019f545
Aditya Keshav <adityakeshav393@gmail.com>
2025-04-08 23:54:54 +0530

    inbuiltClass

---
+++ JS/inbuiltClass.js
@@ -0,0 +1,11 @@
+const date =new Date();
+console.log(date);
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getDay());
+
+
+const map=new Map();
+map.set('name','Alice');
+map.set('age',21);
+console.log(map.get('name'));
