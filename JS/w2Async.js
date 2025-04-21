1d5a33dfd4b2f0326803b2a0b3b330f946824f4c
Aditya Keshav <adityakeshav393@gmail.com>
2025-04-21 20:25:48 +0530

    js

--- JS/w2Async.js
+++ JS/w2Async.js
@@ -1,7 +1,20 @@
-function sum(a, b) {
-	return a + b;
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// let ans = sum(2, 3);
+// console.log("Function definition:", sum);
+// console.log("Function output:", ans);
+
+
+
+function sum(n){
+    let ans=0;
+    for (let i =1;i<=n;i++){
+        ans=ans+i
+    }
+    return ans;
 }
 
-let ans = sum(2, 3);
-console.log("Function definition:", sum);
-console.log("Function output:", ans);
+const ans=sum(100)
+console.log(ans)
